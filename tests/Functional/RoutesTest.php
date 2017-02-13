7c4c653a3b1a3d93d11e053b3280227a1d2d3c28
Marcos Dalte <marcosdalte@gmail.com>
2017-02-13 17:04:47 -0200

    Update RoutesTest.php

--- tests/Functional/RoutesTest.php
+++ tests/Functional/RoutesTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Functional;
 
-class RoutesTest extends BaseTestCase{
+class RoutesTest extends \PHPUnit_Framework_TestCase{
     public function testIndex(){
         $this->assertEquals('200', '200');
     }
