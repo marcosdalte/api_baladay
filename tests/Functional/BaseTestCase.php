714ab7abee3300738155528b6559caf8cb5927ba
Marcos Dalte <marcosdalte@gmail.com>
2017-02-17 17:19:07 +0000

    creating unit tests for routes api

---
+++ tests/Functional/BaseTestCase.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Functional;
+
+use Slim\App;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Http\Headers;
+use Slim\Http\Environment;
+require __DIR__ . '/../../src/Settings.php';
+
+require __DIR__ . '/../../src/auth.php';
+
+class BaseTestCase extends \PHPUnit_Framework_TestCase {
+    public function runApp($requestMethod, $requestUri, $requestData = null){
+        $environment = Environment::mock(
+            [
+                'REQUEST_METHOD' => $requestMethod,
+                'REQUEST_URI' => $requestUri,
+            ]
+        );
+        
+        $request = Request::createFromEnvironment($environment);
+        if (isset($requestData)) {
+            // $request = $request->withParsedBody($requestData);
+            $request = $request->withHeader('AUTHORIZATION', $requestData);
+        }
+        $response = new Response();
+        
+        $app = new \Slim\App();
+        // Set up dependencies
+        require __DIR__ . '/../../src/dependencies.php';
+        
+        // Register middleware
+        require __DIR__ . '/../../src/middleware.php';
+        
+        // Register routes
+        require __DIR__ . '/../../src/routes.php';
+        $response = $app->process($request, $response);
+        
+        return $response;
+    }
+
+}
