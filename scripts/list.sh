752579a18e0a673944e82c68909af59d366d6e27
Marcos Dalte <marcosdalte@gmail.com>
2017-02-21 17:58:57 +0000

    adjust script

--- scripts/list.sh
+++ scripts/list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -X GET https://slimapi-marcosdalte.c9users.io/33 -H "AUTHORIZATION: keyword api_key:87fd0842be8454da3063361d336189da097ae5830cded00c2c42a6fc39e9f188"
+curl -X GET https://slimapi-marcosdalte.c9users.io/cities -H "AUTHORIZATION: keyword api_key:87fd0842be8454da3063361d336189da097ae5830cded00c2c42a6fc39e9f188"
 echo
